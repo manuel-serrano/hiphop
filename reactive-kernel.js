f0cbd600670c4a8eed87316b8f11332fbdcebb28
Colin Vidal <colin@cvidal.org>
2015-09-18 14:55:21 +0200

    ==

--- reactive-kernel.js
+++ reactive-kernel.js
@@ -1,5 +1,8 @@
 "use strict"
 
+const THEN = 0;
+const ELSE = 1;
+
 function must_be_implemented(context) {
    throw "Runtime error: must be implemented! " + context.constructor.name;
 }
@@ -83,225 +86,117 @@ ReactiveMachine.prototype.run = function() {
    console.log("---- reaction " + seq + " ended ----");
 }
 
+/* Emit = Figure 11.4 page 116 */
 
-
-
-
-
-
-
-
-
-///////////////////////////////////////////////////////////////////////////////
-Statement.prototype.run = function() {
-   must_be_implemented(this);
-}
-
-Statement.prototype.connect = function(out_wire, stmt, in_wire) {
-   var wire = new Wire(this, stmt);
-
-   /* As JS properties are hashed in the object, we can access to w_go, w_X
-      with this[wire] since wire matches to the property name */
-   this[out_wire].push(wire);
-   stmt[in_wire].push(wire);
-}
-
-Statement.prototype.is_set_OR = function(wire) {
-   for (var i in this[wire]) {
-      if (this[wire][i].set)
-	 return true;
-   }
-   return false;
-}
-
-Statement.prototype.is_set_return_OR = function(ret_code)  {
-   for (var i in this.w_k[ret_code])
-      if (this.w_k[ret_code][i].set)
-	 return true;
-   return false;
-}
-
-Statement.prototype.init_return_wire = function(return_code) {
-   if (this.w_k[return_code] == undefined)
-      this.w_k[return_code] = [];
-}
-
-Statement.prototype.connect_direct = function(stmt, wire) {
-   this.connect(wire, stmt, wire);
-}
-
-Statement.prototype.connect_return_input = function(ret_code, stmt, in_wire) {
-   if (ret_code < 0)
-      throw "Invalid code";
-
-   var wire = new Wire(this, stmt);
-
-   this.init_return_wire(ret_code);
-   this.w_k[ret_code].push(wire);
-   stmt[in_wire].push(wire);
-}
-
-Statement.prototype.connect_return_return = function(ret_code1, stmt,
-						     ret_code2) {
-   if (ret_code1 < 0 || ret_code2 < 0)
-      throw "Invalid code";
-
-   var wire = new Wire(this, stmt);
-   this.init_return_wire(ret_code1);
-   this.w_k[ret_code1].push(wire);
-   stmt.init_return_wire(ret_code2);
-   stmt.w_k[ret_code2].push(wire);
-}
-
-Statement.prototype.connect_return_direct = function(stmt, ret_code) {
-   this.connect_return_return(ret_code, stmt, ret_code);
-}
-
-function EmitStatement(signal) {
+function Emit(signal) {
    Statement.call(this);
    this.signal = signal;
 }
 
-EmitStatement.prototype = new Statement();
+Emit.prototype = new Statement();
 
-EmitStatement.prototype.run = function() {
-   if (!this.is_set_OR(GO))
+Emit.prototype.run = function() {
+   this.k[0].set = false;
+   this.signal.set = false;
+   if (!this.go.set)
       return;
 
    this.signal.set = true;
    if (this.signal.emit_cb)
       this.signal.emit_cb();
-   this.w_k[0][0].set = true;
-   this.w_k[0][0].stmt2.run();
-   this.signal.set = false;
-   this.w_k[0][0].set = false;
+   this.k[0].set = true;
 }
 
-function PauseStatement(machine) {
+/* Pause - Figure 11.3 page 115 */
+
+function Pause() {
    Statement.call(this);
    this.reg = false;
-   this.machine = machine;
 }
 
-PauseStatement.prototype = new Statement()
+Pause.prototype = new Statement()
 
-PauseStatement.prototype.run = function() {
-   if (this.is_set_OR(RES) && this.reg) {
-      this.reg = false;
-      this.machine.resume_stmt = null;
-      this.w_k[0][0].set = true;
-      this.w_k[0][0].stmt2.run();
-      this.w_k[0][0].set = false;
-   } else if (this.is_set_OR(GO)) {
+Pause.prototype.run = function() {
+   this.k[0].set = false;
+   this.k[1].set = false;
+   this.sel.set = this.reg;
+
+   if (this.go.set) {
+      if (!this.kill.set )
+	 this.reg = true;
+      this.k[1].set = true;
+   }
+
+   if (this.susp.set && this.sel.set && !this.kill.set)
       this.reg = true;
-      this.machine.resume_stmt = this;
-      this.w_k[1][0].set = true;
-      this.w_k[1][0].stmt2.run();
-      this.w_k[1][0].set = false;
+
+   if (this.res.set && this.reg) {
+      this.reg = false;
+      this.k[0].set = true;
    }
 }
 
-function PresentStatement(signal) {
-   Statement.call(this);
+/* Present test - Figure 11.5 page 117
+   X_in[THEN] represent X_in of then branch
+   X_in[ELSE] represent X_in of else branch
+   It's allowed to have only a then branch */
+
+function Present(signal, then_branch, else_branch) {
+   Circuit.call(this);
    this.signal = signal;
-   this.w_go_then = null;
-   this.w_go_else = null;
+   this.init_internal_wires(THEN, then_branch);
+   if (else_branch != undefined)
+      this.init_internal_wires(ELSE, else_branch);
 }
 
-PresentStatement.prototype = new Statement();
+Present.prototype = new Circuit();
 
-PresentStatement.prototype.connect_then = function(stmt, in_wire) {
-   var wire = new Wire(this, stmt);
-   this.w_go_then = wire;
-   stmt[in_wire].push(wire);
+Present.prototype.init_internal_wires = function(branch, circuit) {
+   this.go_in[branch] = circuit.go = new Wire(this, circuit);
+   this.res_in[branch] = circuit.res = new Wire(this, circuit);
+   this.susp_in[branch] = circuit.susp = new Wire(this, circuit);
+   this.kill_in[branch] = circuit.kill = new Wire(this, circuit);
+
+   for (var i in circuit.k) {
+      if (this.k_in[i] == undefined) {
+	 this.k_in[i] = [];
+	 this.k[i] = [];
+      }
+      this.k_in[i][branch] = circuit.k[i] = new Wire(this, circuit);
+   }
 }
 
-PresentStatement.prototype.connect_else = function(stmt, in_wire) {
-   var wire = new Wire(this, stmt);
-   this.w_go_else = wire;
-   stmt[in_wire].push(wire);
-}
-
-PresentStatement.prototype.connect_then_return = function(stmt, ret_code) {
-   var wire = new Wire(this, stmt);
-   this.w_go_then = wire;
-   stmt.w_k[ret_code].push(wire);
-}
-
-PresentStatement.prototype.connect_else_return = function(stmt, ret_code) {
-   var wire = new Wire(this, stmt);
-   this.w_go_else = wire;
-   stmt.w_k[ret_code].push(wire);
-}
-
-PresentStatement.prototype.run = function() {
-   if (!this.is_set_OR(GO))
-      return;
-
-   var wire;
+Present.prototype.run() {
+   var branch;
 
    if (this.signal.set)
-      wire = this.w_go_then;
+      branch = THEN;
    else
-      wire = this.w_go_else;
+      branch = ELSE;
 
-   wire.set = true;
-   wire.stmt2.run();
-   wire.set = false;
+   this.go_in[branch].go.set = this.go.set;
+   this.res_in[branch].res.set = this.res.set;
+   this.susp_in[branch].susp.set = this.susp.set;
+   this.kill_in[branch].kill.set = this.kill.set;
+
+   this.go_in[branch].stmt_out.run();
+
+   this.sel.set = this.sel_in[branch].set;
+   for (var i in this.k_in)
+      this.k[i] = (this.k_in[i][branch] == undefined ?
+		   false : this.k_in[i][branch];)
 }
 
-function make_loop(begin, end) {
-   end.connect_return_input(0, begin, GO);
+/* Sequence - Figure 11.8 page 120 */
+
+function Sequence() {
 }
 
-function ReactiveMachine() {
-   Statement.call(this);
-   this.seq = -1;
-   this.resume_stmt = null;
-}
-
-ReactiveMachine.prototype = new Statement();
-
-/* GO and RES wires of global environment are always set */
-
-ReactiveMachine.prototype.connect = function(out_wire, stmt, in_wire) {
-   Statement.prototype.connect.call(this, out_wire, stmt, in_wire);
-
-   if (out_wire == GO || out_wire == RES)
-      this[out_wire][0].set = true;
-}
-
-ReactiveMachine.prototype.react = function(seq) {
-   if (seq <= this.seq)
-      return;
-   console.log("---- reaction " + seq + " begin ----");
-
-   this.seq = seq;
-
-   if (this.resume_stmt != null)
-      this.resume_stmt.run();
-   else
-      this.w_go[0].stmt2.run();
-
-   if ((this.is_set_return_OR(1) && resume_stmt == null)
-       || (this.is_set_return_OR(0) && resume_stmt != null))
-      throw "Unconsistent state";
-}
-
-ReactiveMachine.prototype.run = function() {
-   console.log("---- reaction " + this.seq + " ended ----");
-}
+Sequence.prototype = new Circuit();
 
 exports.Signal = Signal;
-exports.EmitStatement = EmitStatement;
-exports.PauseStatement = PauseStatement;
-exports.PresentStatement = PresentStatement;
+exports.Emit = Emit;
+exports.Pause = Pause;
+exports.Present = Present;
+exports.Sequence = Sequence;
 exports.ReactiveMachine = ReactiveMachine;
-exports.make_loop = make_loop;
-
-exports.GO = GO;
-exports.RES = RES;
-exports.SUSP = SUSP;
-exports.KILL = KILL;
-exports.SEL = SEL;
-exports.K = K;
