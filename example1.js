79234b7c5c4434f2a7d6e9701934574ccdbd5029
Colin Vidal <colin@cvidal.org>
2015-09-18 15:51:19 +0200

    --

--- example1.js
+++ example1.js
@@ -1,6 +1,6 @@
 "use strict"
 
-var rk = require("./reactive-kernel.js");
+var reactive = require("./reactive-kernel.js");
 
 /*
   <ReactiveMachine>
@@ -12,34 +12,20 @@ var rk = require("./reactive-kernel.js");
   </ReactiveMachine>
 */
 
-var reactive_machine = new rk.ReactiveMachine();
-var pause_stmt = new rk.PauseStatement(reactive_machine);
-var s = new rk.Signal("S", false, function() {
-   console.log("EMIT " + this.name)
+var sigS = new reactive.Signal("S", false, function() {
+   console.out("EMIT S");
 });
-var t = new rk.Signal("T", false, function() {
-   console.log("EMIT " + this.name)
+
+var sigT = new reactive.Signal("T", false, function() {
+   console.out("EMIT T");
 });
-var present_stmt = new rk.PresentStatement(s);
-var emit_stmt = new rk.EmitStatement(s);
-var emitT = new rk.EmitStatement(t);
 
-reactive_machine.connect_direct(pause_stmt, rk.GO);
-reactive_machine.connect_direct(pause_stmt, rk.RES);
+var emitS = new reactive.Emit(sigS);
+var emitT = new reactive.Emit(sigT);
+var present = new reactive.Present(sigS, emitT);
+var pause = new Pause();
+var seq = new Sequence(pause, emitS, present);
+var machine = new ReactiveMachine(seq);
 
-pause_stmt.connect_return_direct(reactive_machine, 1);
-pause_stmt.connect_return_input(0, emit_stmt, rk.GO);
-
-emit_stmt.connect_return_input(0, present_stmt, rk.GO);
-present_stmt.connect_then(emitT, rk.GO);
-present_stmt.connect_else_return(reactive_machine, 0);
-
-emitT.connect_return_direct(reactive_machine, 0);
-
-
-console.log("reactive machine ready...");
-
-reactive_machine.react(0);
-
-reactive_machine.react(1);
-
+for (var i = 0; i < 5; i++)
+   machime.react(i);
