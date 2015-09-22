25277511e9e639f8b2e50b781ae6740e9ed5ccf5
Colin Vidal <colin@cvidal.org>
2015-09-22 16:51:07 +0200

    tri topologique marche

--- example-parallel2.js
+++ example-parallel2.js
@@ -10,10 +10,8 @@ var sigJ = new reactive.Signal("J", false, function() {
 
 var emitI = new reactive.Emit(sigI);
 var emitJ = new reactive.Emit(sigJ);
-var awaitI = new reactive.Await(sigI);
 var present = new reactive.Present(sigI, emitJ);
-var seq = new reactive.Sequence(awaitI, present);
-var par = new reactive.Parallel(seq, emitI);
+var par = new reactive.Parallel(present, emitI);
 var machine = new reactive.ReactiveMachine(par);
 
 machine.react();
