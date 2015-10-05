3dda896c2b0637eeb3fe645baba7074733a5505e
Colin Vidal <colin.vidal@inria.fr>
2015-10-05 11:36:46 +0200

    impl. sched - tests

---
+++ tests/example-parallel2.js
@@ -0,0 +1,22 @@
+var reactive = require("../reactive-kernel.js");
+var inspector = require("../inspector.js");
+
+var sigI = new reactive.Signal("I", false, function() {
+   console.log("EMIT I");
+});
+
+var sigJ = new reactive.Signal("J", false, function() {
+   console.log("EMIT J");
+});
+
+var emitI = new reactive.Emit(sigI);
+var emitJ = new reactive.Emit(sigJ);
+var present = new reactive.Present(sigI, emitJ);
+var par = new reactive.Parallel(present, emitI);
+var machine = new reactive.ReactiveMachine(par);
+
+machine.react();
+//(new inspector.Inspector(machine)).inspect();
+machine.react();
+machine.react();
+machine.react();
