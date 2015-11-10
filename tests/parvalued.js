3e40d481b257ca1dc85a8e5597e568d54ddba9c3
Colin Vidal <colin.vidal@inria.fr>
2015-11-10 13:32:24 +0100

    --

---
+++ tests/parvalued.js
@@ -0,0 +1,20 @@
+var reactive = require("../reactive-kernel.js");
+var rjs = require("../xml-compiler.js");
+var inspector = require("../inspector.js");
+
+var sigJ = new reactive.ValuedSignal("J", "integer");
+
+var machine = <rjs.ReactiveMachine name="parvalued">
+  <rjs.outputsignal ref=${sigJ}/>
+  <rjs.localsignal signal_name="I" type="integer">
+    <rjs.Parallel>
+      <rjs.present signal_name="I">
+	<rjs.emit signal_name="J"
+		  expr=${<rjs.sigexpr signal_name="I"/>} />
+      </rjs.present>
+      <rjs.emit signal_name="I" expr=5 />
+    </rjs.Parallel>
+  </rjs.localsignal>
+</rjs.ReactiveMachine>;
+
+exports.prg = machine;
