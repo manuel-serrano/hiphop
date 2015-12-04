a84268c2cef7065ea14e0292b73378127bb26a03
Colin Vidal <colin.vidal@inria.fr>
2015-12-04 20:28:04 +0100

    suspend OK - pretty printer todo

---
+++ tests/suspend.js
@@ -0,0 +1,22 @@
+"use hopscript"
+
+var rjs = require("reactive-js");
+
+var prg = <rjs.reactivemachine debug name="SUSPEND">
+  <rjs.inputsignal name="I"/>
+  <rjs.outputsignal name="J"/>
+  <rjs.outputsignal name="O"/>
+  <rjs.sequence>
+    <rjs.suspend signal_name="I">
+      <rjs.loop>
+	<rjs.sequence>
+	  <rjs.emit signal_name="O"/>
+	  <rjs.pause/>
+	</rjs.sequence>
+      </rjs.loop>
+    </rjs.suspend>
+    <rjs.emit signal_name="J"/>
+  </rjs.sequence>
+</rjs.reactivemachine>;
+
+exports.prg = prg;
