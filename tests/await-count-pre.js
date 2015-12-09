362dd1517669830eef85b67f824d838b75aa61fa
Colin Vidal <colin.vidal@inria.fr>
2015-12-09 13:21:13 +0100

    --

---
+++ tests/await-count-pre.js
@@ -0,0 +1,17 @@
+"use hopscript"
+
+var hh = require("hiphop");
+
+var prg =
+    <hh.reactivemachine debug name="await3">
+      <hh.inputsignal name="I"/>
+      <hh.outputsignal name="O"/>
+      <hh.loop>
+	<hh.sequence>
+	  <hh.await signal_name="I" count=3 test_pre />
+	  <hh.emit signal_name="O" />
+	</hh.sequence>
+      </hh.loop>
+    </hh.reactivemachine>;
+
+exports.prg = prg;
