49cc8d90901a9a36819761fa1c5357d41c46cbea
Colin Vidal <colin.vidal@inria.fr>
2015-12-17 11:23:42 +0100

    fix bugs and add tests

---
+++ tests/cross-await.js
@@ -0,0 +1,25 @@
+"use hopscript"
+
+var hh = require("hiphop");
+
+var prg =
+    <hh.reactivemachine debug name="crossawait">
+      <hh.outputsignal name="A"/>
+      <hh.outputsignal name="B"/>
+      <hh.outputsignal name="END1"/>
+      <hh.outputsignal name="END2"/>
+      <hh.parallel>
+	<hh.sequence>
+	  <hh.emit signal_name="A"/>
+	  <hh.await immediate signal_name="B"/>
+	  <hh.emit signal_name="END1"/>
+	</hh.sequence>
+	<hh.sequence>
+	  <hh.emit signal_name="B"/>
+	  <hh.await immediate signal_name="A"/>
+	  <hh.emit signal_name="END2"/>
+	</hh.sequence>
+      </hh.parallel>
+    </hh.reactivemachine>;
+
+exports.prg = prg;
