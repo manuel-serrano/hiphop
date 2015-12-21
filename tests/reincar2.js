f8c60ced9ceb1614eef387cf4bed70542275068a
Colin Vidal <colin.vidal@inria.fr>
2015-12-21 17:27:50 +0100

    --

---
+++ tests/reincar2.js
@@ -0,0 +1,44 @@
+"use hopscript"
+
+var hh = require("hiphop");
+
+function plus(a, b) {
+   return a + b;
+}
+
+var prg =
+    <hh.reactivemachine debug name="reincar2">
+      <hh.outputsignal name="S" type="number"/>
+      <hh.outputsignal name="I"/>
+      <hh.outputsignal name="J" type="number"/>
+      <hh.loop>
+	<hh.parallel>
+
+	  <hh.localsignal name="M"
+			  init_value=5
+			  combine_with="+"
+			  type="number">
+	    <hh.emit signal_name="J" exprs=${hh.value("M")}/>
+	    <hh.pause/>
+	    <hh.emit signal_name="M" exprs=5 />
+	  </hh.localsignal>
+
+	  <hh.localsignal name="N">
+	    <hh.present signal_name="N">
+	      <hh.emit signal_name="I"/>
+	    </hh.present>
+	    <hh.pause/>
+	    <hh.emit signal_name="N"/>
+	  </hh.localsignal>
+
+	  <hh.localsignal name="L" type="number">
+	    <hh.emit signal_name="L" exprs=4 />
+	    <hh.pause/>
+	    <hh.emit signal_name="S" func=${plus} exprs=${[hh.value("L"), 5]}/>
+	  </hh.localsignal>
+
+	</hh.parallel>
+      </hh.loop>
+    </hh.reactivemachine>;
+
+exports.prg = prg;
