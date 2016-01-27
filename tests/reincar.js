4df5d5b4e47d21ba10064098efbebacc26aa99b1
Colin Vidal <colin.vidal@inria.fr>
2016-01-27 14:17:59 +0100

    --

--- tests/reincar.js
+++ tests/reincar.js
@@ -6,13 +6,11 @@ var prg = <hh.module>
   <hh.outputsignal name="O"/>
   <hh.loop>
     <hh.localsignal name="S">
-      <hh.sequence>
-	<hh.present signal_name="S">
-	  <hh.emit signal_name="O"/>
-	</hh.present>
-	<hh.pause/>
-	<hh.emit signal_name="S"/>
-      </hh.sequence>
+      <hh.present signal_name="S">
+	<hh.emit signal_name="O"/>
+      </hh.present>
+      <hh.pause/>
+      <hh.emit signal_name="S"/>
     </hh.localsignal>
   </hh.loop>
 </hh.module>;
