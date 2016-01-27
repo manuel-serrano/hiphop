997acad7fa7b78f5575ff8c9cff60943111ff0f2
Colin Vidal <colin.vidal@inria.fr>
2016-01-27 17:16:48 +0100

    --

--- tests/emitvaluedlocal2.js
+++ tests/emitvaluedlocal2.js
@@ -1,24 +1,20 @@
 "use hopscript"
 
-var rjs = require("hiphop");
+var hh = require("hiphop");
 
 function sum(arg1, arg2) {
    return arg1 + arg2;
 }
 
-var prg = <rjs.ReactiveMachine debug name="emitvaluedlocal2">
-  <rjs.outputsignal name="O" type="number"/>
-  <rjs.loop>
-    <rjs.sequence>
-      <rjs.localsignal name="S" type="number" init_value=1 >
-	<rjs.sequence>
-	  <rjs.emit signal_name="S" func=${sum} exprs=${[rjs.preValue("S"),1]}/>
-	  <rjs.emit signal_name="O" exprs=${rjs.value("S")}/>
-	</rjs.sequence>
-      </rjs.localsignal>
-      <rjs.pause/>
-    </rjs.sequence>
-  </rjs.loop>
-</rjs.ReactiveMachine>;
+var prg = <hh.module>
+  <hh.outputsignal name="O" type="number"/>
+  <hh.loop>
+    <hh.localsignal name="S" type="number" init_value=1 >
+      <hh.emit signal_name="S" func=${sum} exprs=${[hh.preValue("S"),1]}/>
+      <hh.emit signal_name="O" exprs=${hh.value("S")}/>
+    </hh.localsignal>
+    <hh.pause/>
+  </hh.loop>
+</hh.module>
 
-exports.prg = prg;
+exports.prg = new hh.ReactiveMachine(prg, "emitvaluedlocal2");
