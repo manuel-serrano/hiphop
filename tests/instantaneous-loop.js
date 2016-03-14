a9dc3d60251b4827daa14448466dfa1e583cb7a6
Colin Vidal <colin.vidal@inria.fr>
2016-03-14 18:20:53 +0100

    --

--- tests/instantaneous-loop.js
+++ tests/instantaneous-loop.js
@@ -4,11 +4,11 @@ var hh = require("hiphop");
 
 try {
    var prg = <hh.module>
-     <hh.outputsignal name="O" type="string"/>
+     <hh.outputsignal name="O" valued/>
      <hh.loop>
-       <hh.localsignal name="L" type="string">
-	 <hh.emit signal_name="L" exprs=${"foo bar"}/>
-	 <hh.emit signal_name="O" exprs=${hh.value("L")}/>
+       <hh.localsignal name="L" valued>
+	 <hh.emit signal_name="L" args=${"foo bar"}/>
+	 <hh.emit signal_name="O" args=${hh.value("L")}/>
        </hh.localsignal>
      </hh.loop>
    </hh.module>;
