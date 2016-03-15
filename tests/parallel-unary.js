97f1bdec9e4bc90dd8bf7a034097fdcf965f7f4b
Colin Vidal <colin.vidal@inria.fr>
2016-03-15 17:00:28 +0100

    --

--- tests/parallel-unary.js
+++ tests/parallel-unary.js
@@ -2,21 +2,22 @@
 
 var hh = require("hiphop");
 
-var prg = <hh.module>
-  <hh.outputsignal name="O"/>
-  <hh.loop>
-    <hh.localsignal name="L">
-      <hh.parallel>
-	<hh.emit signal_name="L"/>
-	<hh.parallel>
-	  <hh.present signal_name="L">
-	    <hh.emit signal_name="O"/>
-	  </hh.present>
-	</hh.parallel>
-      </hh.parallel>
-    </hh.localsignal>
-    <hh.pause/>
-  </hh.loop>
-</hh.module>;
+var prg =
+    <hh.module>
+      <hh.outputsignal name="O"/>
+      <hh.loop>
+	<hh.localsignal name="L">
+	  <hh.parallel>
+	    <hh.emit signal_name="L"/>
+	    <hh.parallel>
+	      <hh.present signal_name="L">
+		<hh.emit signal_name="O"/>
+	      </hh.present>
+	    </hh.parallel>
+	  </hh.parallel>
+	</hh.localsignal>
+	<hh.pause/>
+      </hh.loop>
+    </hh.module>;
 
 exports.prg = new hh.ReactiveMachine(prg, "parallelunary");
