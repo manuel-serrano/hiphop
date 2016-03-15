7110cd831bc4af6988c49e2f22899b282d84e4c3
Colin Vidal <colin.vidal@inria.fr>
2016-03-15 14:28:07 +0100

    add type pure/valued check on signal

--- tests/await-valued.js
+++ tests/await-valued.js
@@ -3,16 +3,14 @@
 var hh = require("hiphop");
 
 function foo(evt) {
-   console.log("foo called by", evt.signal, "with value", evt.value);
+   console.log("foo called by", evt.signalName, "with value", evt.signalValue);
 }
 
 var prg = <hh.module>
-  <hh.inputsignal name="I" type="number"/>
-  <hh.outputsignal name="O" type="number"/>
-  <hh.sequence>
+  <hh.inputsignal name="I" valued />
+  <hh.outputsignal name="O" valued />
     <hh.await signal_name="I" />
-    <hh.emit signal_name="O" exprs=${hh.value("I")}/>
-  </hh.sequence>
+    <hh.emit signal_name="O" args=${hh.value("I")}/>
 </hh.module>;
 
 var m = new hh.ReactiveMachine(prg, "awaitvalued");
