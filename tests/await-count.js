e383cbce3ac0515f511a8891e659e657e56a34c4
Colin Vidal <colin.vidal@inria.fr>
2016-03-15 13:33:27 +0100

    --

--- tests/await-count.js
+++ tests/await-count.js
@@ -7,11 +7,10 @@ var prg =
       <hh.inputsignal name="I"/>
       <hh.outputsignal name="O"/>
       <hh.loop>
-	<hh.sequence>
-	  <hh.await signal_name="I" count=3 />
-	  <hh.emit signal_name="O" />
-	</hh.sequence>
+	<hh.await signal_name="I" count=3 />
+	<hh.emit signal_name="O" />
       </hh.loop>
     </hh.module>;
 
 exports.prg = new hh.ReactiveMachine(prg, "await3");
+
