675de82e78105b8bc6775256e3bacc2456e049db
Colin Vidal <colin.vidal@inria.fr>
2016-03-16 15:20:52 +0100

    --

--- tests/force-pre.js
+++ tests/force-pre.js
@@ -4,7 +4,7 @@ var hh = require("hiphop");
 
 try {
 var prg = <hh.module>
-  <hh.outputsignal name="O" type="number" init_value=0 />
-  <hh.emit signal_name="O" exprs=${hh.value("O")}/>
+  <hh.outputsignal name="O" init_value=0 />
+  <hh.emit signal_name="O" args=${hh.value("O")}/>
 </hh.module>;
 }catch(e){console.log(e.message)}
