a3a4245a7850c9b83e5c32d3660a0712607e4408
Colin Vidal <colin.vidal@inria.fr>
2016-03-17 13:28:56 +0100

    --

--- tests/prepure2.js
+++ tests/prepure2.js
@@ -7,14 +7,12 @@ var prg = <hh.module>
   <hh.outputsignal name="O2"/>
   <hh.localsignal name="S">
     <hh.loop>
-      <hh.sequence>
-	<hh.present test_pre signal_name="S">
-	  <hh.emit signal_name="O1"/>
-	  <hh.emit signal_name="O2"/>
-	</hh.present>
-	<hh.pause/>
-	<hh.emit signal_name="S"/>
-      </hh.sequence>
+      <hh.present test_pre signal_name="S">
+	<hh.emit signal_name="O1"/>
+	<hh.emit signal_name="O2"/>
+      </hh.present>
+      <hh.pause/>
+      <hh.emit signal_name="S"/>
     </hh.loop>
   </hh.localsignal>
 </hh.module>;
