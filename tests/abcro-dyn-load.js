a68e9922d09204f60fda6caaf57190a3d9979fd3
Colin Vidal <colin.vidal@inria.fr>
2016-03-17 15:16:55 +0100

    dynamic loading

--- tests/abcro-dyn-load.js
+++ tests/abcro-dyn-load.js
@@ -1,7 +1,7 @@
 "require  hopscript"
 
-console.error("Dyn load of statement NYI");
-process.exit(1);
+// console.error("Dyn load of statement NYI");
+// process.exit(1);
 
 const hh = require("hiphop");
 
@@ -12,16 +12,19 @@ const prg =
 	<hh.inputsignal name="C"/>
 	<hh.inputsignal name="R"/>
 	<hh.outputsignal name="O"/>
+	<hh.outputsignal name="AUX"/>
 	<hh.localsignal name="X">
 	  <hh.emit signal_name="X"/>
 	</hh.localsignal>
 	<hh.loopeach signal_name="R">
-	  <hh.parallel>
-	    <hh.await signal_name="A"/>
-	    <hh.await  signal_name="B"/>
-	    <hh.await signal_name="C"/>
-	  </hh.parallel>
-	  <hh.emit id=7 signal_name="O"/>
+	  <hh.sequence id="seq">
+	    <hh.parallel>
+	      <hh.await signal_name="A"/>
+	      <hh.await  signal_name="B"/>
+	      <hh.await signal_name="C"/>
+	    </hh.parallel>
+	    <hh.emit signal_name="O"/>
+	  </hh.sequence>
 	</hh.loopeach>
       </hh.module>;
 
@@ -30,21 +33,22 @@ const machine = new hh.ReactiveMachine(prg, "ABCRO-DYN-LOAD");
 console.log(machine.react());
 machine.setInput("A", undefined);
 
-machine.insertSignal(<hh.outputSignal name="D"/>);
+//machine.insertSignal(<hh.outputSignal name="D"/>);
 
 console.log(machine.react());
 machine.setInput("B", undefined);
 machine.setInput("C", undefined);
 console.log(machine.react());
 
-machine.insertAfter(machine.getElementById("7"),
-		    <hh.localsignal name="Z">
-		      <hh.emit signal_name="Z"/>
-		      <hh.present signal_name="Z">
-			<hh.emit signal_name="D"/>
-		      </hh.present>
-		    </hh.localsignal>
-		   );
+let seq = machine.getElementById("seq");
+
+seq.appendChild(
+   <hh.localsignal name="Z">
+     <hh.emit signal_name="Z"/>
+     <hh.present signal_name="Z">
+       <hh.emit signal_name="AUX"/>
+     </hh.present>
+   </hh.localsignal>);
 
 console.log(machine.react());
 machine.setInput("R", undefined);
