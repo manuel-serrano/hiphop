a7b79c718145742f5080359509319d4ce0e3947b
Colin Vidal <colin.vidal@inria.fr>
2016-03-23 15:14:12 +0100

    --

---
+++ tests/remove-branch.js
@@ -0,0 +1,43 @@
+"use hopscript"
+
+const hh = require("hiphop");
+
+const prg =
+      <hh.module>
+	<hh.outputsignal name="O" combine_with=${(x, y) => x + y}/>
+	<hh.loop>
+	  <hh.parallel id="par">
+	    <hh.emit signal_name="O" args=1/>
+	  </hh.parallel>
+	  <hh.pause/>
+	  <hh.pause/>
+	</hh.loop>
+      </hh.module>;
+
+function add_emit(machine) {
+   let branch = <hh.emit signal_name="O" args=1/>;
+
+   machine.getElementById("par").appendChild(branch);
+   return branch;
+}
+
+const machine = new hh.ReactiveMachine(prg, "incr-branch");
+
+console.log(machine.react());
+console.log(machine.react());
+console.log(machine.react());
+console.log(machine.react());
+let br1 = add_emit(machine);
+console.log(machine.react());
+console.log(machine.react());
+console.log(machine.react());
+add_emit(machine);
+add_emit(machine);
+add_emit(machine);
+console.log(machine.react());
+console.log(machine.react());
+
+machine.getElementById("par").removeChild(br1);
+
+console.log(machine.react());
+console.log(machine.react());
