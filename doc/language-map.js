77ae98be37b8d995cbaf134160f5af3266df637d
Colin Vidal <colin.vidal@inria.fr>
2016-04-07 11:14:28 +0200

    --

--- doc/language-map.js
+++ doc/language-map.js
@@ -39,7 +39,7 @@ function INDENT() {
    return ret;
 }
 
-function TERM_NODE(attrs) {
+function TERM_CNODE(attrs) {
    return <span>
      <strong>&lt;${attrs.name}&gt;</strong>
      ${(function() {if (attrs.br) return <br/>})()}
@@ -49,24 +49,45 @@ function TERM_NODE(attrs) {
    </span>;
 }
 
+function TERM_NODE(attrs) {
+   return <span>
+     <strong>&lt;${attrs.name} </strong>
+     ${(function() {if (attrs.br) return <br/>})()}
+     ${children(arguments)}
+     ${(function() {if (attrs.br) return <br/>})()}
+     <strong> /&gt;</strong>
+   </span>;
+}
+
 function TERM(attrs) {
    return <span>
-     ${attrs.name}
-     ${(function() {if (attrs.br) return <br/>})()}
+     ${(function() {
+	if (!attrs)
+	   "";
+	if (attrs.JSFunction)
+	   return <span><strong>${attrs.name}</strong>=JSFunction</span>;
+	else if (attrs.JSString)
+	   return <span><strong>${attrs.name}</strong>=JSString</span>;
+	else if (attrs.JSValue)
+	   return <span><strong>${attrs.name}</strong>=JSValue</span>;
+	else
+	   return <span><strong>${attrs.name}</strong></span>;
+     })()}
+     ${(function() {if (attrs && attrs.br) return <br/>})()}
    </span>
 }
 
 function NTERM(attrs) {
    return <em style=${"font-size:140%;"}>
      ${attrs.name}
-     ${(function() {if (attrs.br) return <br/>})()}
+     ${(function() {if (attrs && attrs.br) return <br/>})()}
    </em>
 }
 
 function OPT(attrs) {
    return <span>
      [ ${children(arguments)} ]
-     ${(function() {if (attrs.br) return <br/>})()}
+     ${(function() {if (attrs && attrs.br) return <br/>})()}
    </span>;
 }
 
@@ -76,20 +97,55 @@ exports.langage_map =
      ${header("Statements", "stat")}
      ${header("Expressions")}
      ${header("Interface")}
+
      ${header("Module")}
      <tr>
        <td>
-	 <term_node name="Module">
+	 <term_cnode name="Module">
 	   <indent>
 	     <opt br>
 	       <nterm name="module-header"/>
 	     </opt>
 	     <nterm name="stmt"/>
 	   </indent>
-	 </term_node>
+	 </term_cnode>
        </td>
        <td>
 	 Entry point of a reactive program.
+       </td>
+     </tr>
+     ${header("Module header", "module-header")})
+     <tr>
+       <td colspan=2>
+	 <term_node name="InputSignal">
+	   <indent>
+	     <term br name="name" JSString />
+	     <opt br><term name="valued" /></opt>
+	     <opt br><term name="combine_with" JSFunction /></opt>
+	     <opt br><term name="init_value" JSValue /></opt>
+	     <opt br><term name="reinit_func" JSFunction /></opt>
+	   </indent>
+         </term_node>
+       </td>
+     </tr>
+     <tr>
+       <td>
+	 <term_node name="OutputSignal">
+	   <indent>
+	     <term br name="name" JSString />
+	     <opt br><term name="valued" /></opt>
+	     <opt br><term name="combine_with" JSFunction /></opt>
+	     <opt br><term name="init_value" JSValue /></opt>
+	     <opt br><term name="reinit_func" JSFunction /></opt>
+	   </indent>
+         </term_node>
+       </td>
+       <td>
+	 <ul>
+	   <li>bla bla 1 sur output signal</li>
+	   <li>bla bla 2 sur output signal</li>
+	   <li>bla bla 3 sur output signal</li>
+	 </ul>
        </td>
      </tr>
    </table>
