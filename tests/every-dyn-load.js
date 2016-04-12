49c9b6bffa9e306fe2311f49b31a5716384dc2cf
Colin Vidal <colin.vidal@inria.fr>
2016-04-12 11:06:11 +0200

    fix dynamic load register bug

---
+++ tests/every-dyn-load.js
@@ -0,0 +1,72 @@
+"use hopscript"
+
+const hh = require("hiphop");
+const fs = require("fs");
+
+function make_atom(i) {
+   return <hh.loopeach signal_name="GO">
+     <hh.atom func=${function() {console.log("branch", i)}}/>
+   </hh.loop>;
+}
+
+function make_atom2(i) {
+   return <hh.loop>
+     <hh.await immediate signal_name="GO"/>
+     <hh.atom func=${function() {console.log("branch", i)}}/>
+     <hh.pause/>
+   </hh.loop>;
+}
+
+function make_atom3(i) {
+   return <hh.every immediate signal_name="GO">
+     <hh.atom func=${function() {console.log("branch", i)}}/>
+   </hh.every>;
+}
+
+const prg =
+      <hh.module>
+	<hh.localsignal name="GO">
+	  <hh.parallel id="par">
+	    <hh.loop>
+	      <hh.emit signal_name="GO"/>
+	      <hh.pause/>
+	    </hh.loop>
+
+	    ${make_atom(0)}
+	    ${make_atom2(4)}
+	  </hh.parallel>
+	</hh.localsignal>
+      </hh.module>;
+
+var machine = new hh.ReactiveMachine(prg, "");
+
+//console.log(machine.ast.pretty_print());
+
+console.log(machine.react());
+console.log(machine.react());
+
+console.log("add 1");
+machine.getElementById("par").appendChild(make_atom2(1));
+console.log(machine.react());
+console.log(machine.react());
+
+//machine.trace_on = 2;
+
+console.log("add 2");
+machine.getElementById("par").appendChild(make_atom(2));
+//console.error(machine.ast.pretty_print());
+console.log(machine.react());
+//console.error(machine.ast.pretty_print());
+
+machine.trace_on = 0;
+
+console.log(machine.react());
+console.log(machine.react());
+
+console.log("add 3");
+machine.getElementById("par").appendChild(make_atom3(3));
+console.log(machine.react());
+console.log(machine.react());
+console.log(machine.react());
+
+//console.log(machine.ast.pretty_print());
