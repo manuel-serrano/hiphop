fde2c8c0a1450ca3254e04e79426b7f05e98beed
Colin Vidal <colin.vidal@inria.fr>
2016-06-15 16:39:26 +0200

    exec: autoreact NONE/ANY/ALL in exec_interface

---
+++ examples/translator1.js
@@ -0,0 +1,76 @@
+"use hopscript"
+
+service translator() {
+   return <html>
+     <head module=${"hiphop"}>
+       ~{
+	  var hh;
+	  var m;
+	  var en, enColor;
+	  var es, esColor;
+
+	  window.onload = function() {
+	     hh = require("hiphop");
+
+	     function EXECCOLOR( attr, body ) {
+		return <hh.sequence>
+		  <hh.emit signal_name=${`${attr.lang}-color`} arg="red"/>
+		  <hh.exec signal_name=${`${attr.lang}-out`}
+			   interface=${exec_interface}
+			   arg0=${attr.lang}
+			   arg1=${hh.value("fr-in")}/>
+		  <hh.emit signal_name=${`${attr.lang}-color`} arg="blue"/>
+		</hh.sequence>
+	     }
+
+	     const exec_interface = {
+		start: function(lang, totranslate) {
+		   var xhttp = new XMLHttpRequest();
+		   var svc = "http://mymemory.translated.net/api/get";
+		   var opt = "?langpair=fr|" + lang + "&q=" + totranslate;
+
+		   xhttp.onreadystatechange = function() {
+		      if (xhttp.readyState == 4 && xhttp.status == 200) {
+			 let res = JSON.parse(xhttp.responseText);
+			 let trad = res.responseData.translatedText
+
+			 this.return(trad);
+		      }
+		   }.bind(this);
+		   xhttp.open("GET", svc + opt, true);
+		   xhttp.send();
+		},
+		autoreact: hh.ANY
+	     };
+
+	     m =  new hh.ReactiveMachine(
+		<hh.module>
+		  <hh.inputsignal name="fr-in" combine_with=${(a, b) => b}/>
+		  <hh.outputsignal name="en-out" init_value="??"/>
+		  <hh.outputsignal name="en-color" init_value="green"/>
+                  <hh.outputsignal name="es-out" init_value="Â¿?"/>
+		  <hh.outputsignal name="es-color" init_value="green"/>
+
+		  <hh.loopeach signal_name="fr-in">
+		    <hh.parallel>
+		      <execColor lang="en"/>
+		      <execColor lang="es"/>
+		    </hh.parallel>
+		  </hh.loopeach>
+		</hh.module>
+	     );
+
+	     en = m.reactProxy("en-out");
+	     es = m.reactProxy("es-out");
+	     enColor = m.reactProxy("en-color");
+	     esColor = m.reactProxy("es-color");
+	  }
+       }
+     </head>
+     <body>
+       <input type="text" oninput=~{m.inputAndReact("fr-in", this.value)}/>
+       <div style=~{`color: ${enColor.value}`}>english: <react>~{en.value}</react></div>
+       <div style=~{`color: ${esColor.value}`}>spanish: <react>~{es.value}</react></div>
+     </body>
+   </html>;
+}
