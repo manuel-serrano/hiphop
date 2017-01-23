29497955f3ac550679f93f7899805c8b5d7e7bb4
Colin Vidal <colin.vidal@inria.fr>
2017-01-23 11:08:13 +0100

    update examples

--- examples/translator.js
+++ examples/translator.js
@@ -8,14 +8,6 @@ service translator() {
      ~{
 	var hh;
 	var m;
-	var colorEnp;
-        var transEnp;
-        var colorPne;
-        var transPne;
-        var colorEsp;
-        var transEsp;
-        var colorPse;
-        var transPse;
 
 	function translate() {
 	   var req = new XMLHttpRequest();
@@ -28,10 +20,10 @@ service translator() {
 	   req.send();
 	}
 
-	function execColor(lang, _await=false) {
+	function execColor(lang) {
 	   return <hh.module lang=${{initValue: lang}} color text trans>
 		<hh.emit color value=${"red"}/>
-		${_await ? <hh.await text/> : <hh.nothing/>}
+	        <hh.await immediate text/>
 		<hh.exec trans apply=${translate}/>
 		<hh.emit color value=${"green"}/>
 	     </hh.module>
@@ -44,41 +36,32 @@ service translator() {
 		<hh.loopeach text>
 		  <hh.parallel>
 		    <hh.run module=${execColor("fr|en")} color=colorEn trans=transEn/>
-		    <hh.run module=${execColor("en|fr", true)} color=colorNe text=transEn trans=transNe/>
+		    <hh.run module=${execColor("en|fr")} color=colorNe text=transEn trans=transNe/>
 		    <hh.run module=${execColor("fr|es")} color=colorEs trans=transEs/>
-		    <hh.run module=${execColor("es|fr", true)} color=colorSe text=transEs trans=transSe/>
+		    <hh.run module=${execColor("es|fr")} color=colorSe text=transEs trans=transSe/>
 		  </hh.parallel>
 		</hh.loopeach>
               </hh.module>);
-
-	   colorEnp = m.reactProxy("colorEn");
-           transEnp = m.reactProxy("transEn");
-           colorPne = m.reactProxy("colorNe");
-           transPne = m.reactProxy("transNe");
-           colorEsp = m.reactProxy("colorEs");
-           transEsp = m.reactProxy("transEs");
-           colorPse = m.reactProxy("colorSe");
-           transPse = m.reactProxy("transSe");
 	}
      }
      </head>
      <style> * { font-size: 60px; color: black; }</style>
      <body>
-       <input oninput=~{m.inputAndReact("text", this.value)}/>
+       <input oninput=~{m.text = this.value}/>
        <div>
-         <span style=~{`color: ${colorEnp.value}`}>
-           <react>~{transEnp.value}</react>
+         <span style=~{`color: ${m.colorEn}`}>
+           <react>~{m.transEn}</react>
          </span> -
-         <span style=~{`color: ${colorPne.value}`}>
-           <react>~{transPne.value}</react>
+         <span style=~{`color: ${m.colorNe}`}>
+           <react>~{m.transNe}</react>
          </span>
        </div>
        <div>
-         <span style=~{`color: ${colorEsp.value}`}>
-           <react>~{transEsp.value}</react>
+         <span style=~{`color: ${m.colorEs}`}>
+           <react>~{m.transEs}</react>
          </span> -
-         <span style=~{`color: ${colorPse.value}`}>
-           <react>~{transPse.value}</react>
+         <span style=~{`color: ${m.colorSe}`}>
+           <react>~{m.transSe}</react>
          </span>
        </div>
      </body>
