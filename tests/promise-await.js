9d145e2afc6fb62f1c615d6d9a4ca4c093d9cb82
Colin Vidal <colin.vidal@inria.fr>
2017-01-25 14:09:15 +0100

    Allow to set promises on output signals

---
+++ tests/promise-await.js
@@ -0,0 +1,21 @@
+"use hopscript"
+
+const hh = require("hiphop");
+const tl = hh.timelib;
+
+const m = new hh.ReactiveMachine(
+   <hh.module I O>
+     <hh.await immediate I/>
+     <tl.timeout value=1000/>
+     <hh.emit O apply=${function() { return this.value.I }}/>
+   </hh.module>
+);
+
+async function foo() {
+   m.I = 45;
+   console.log("I emitted...");
+   let o  = await m.promise.O;
+   console.log("O(" + o + ") emitted...");
+}
+
+foo();
