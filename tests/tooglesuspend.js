3110a06a7dec692a1e143d439ca67732440dc477
Colin Vidal <colin.vidal@inria.fr>
2017-05-09 15:30:45 +0200

    Lang: add suspend tooglesignal constructor.

---
+++ tests/tooglesuspend.js
@@ -0,0 +1,27 @@
+"use hopscript"
+
+const hh = require("hiphop");
+
+const mach = new hh.ReactiveMachine(
+   <hh.module toogle>
+     <hh.suspend toogleSignal=toogle>
+       <hh.loop>
+	 <hh.atom apply=${function() {
+	    console.log("plop");
+	 }}/>
+	 <hh.pause/>
+       </hh.loop>
+     </hh.suspend>
+   </hh.module>
+);
+
+mach.debug_emitted_func = console.log;
+
+mach.react();
+mach.react();
+mach.inputAndReact('toogle');
+mach.react();
+mach.react();
+mach.inputAndReact('toogle');
+mach.react();
+mach.react();
