6f1be5f3e9483970ee8987d8765db0d5fe9c073b
Colin Vidal <colin.vidal@inria.fr>
2017-05-11 14:10:30 +0200

    Compiler: Exec: call kill handler in case of abortion.

---
+++ tests/every-exec-kill.js
@@ -0,0 +1,26 @@
+"use hopscript"
+
+const hh = require("hiphop");
+const mach = new hh.ReactiveMachine(
+   <hh.module S>
+     <hh.every S>
+       <hh.atom apply=${() => console.log("every")}/>
+       <hh.exec apply=${function() {
+	  console.log("start", this.id);
+	    setTimeout(this.notifyAndReact, 500);
+	 }}
+		  kill=${function() {
+		     console.log("killed", this.id);
+		  }}
+
+      />
+
+     </hh.every>
+   </hh.module>
+);
+
+mach.react();
+console.log('----');
+mach.inputAndReact("S");
+console.log('----');
+setTimeout(() => mach.inputAndReact("S"), 200);
