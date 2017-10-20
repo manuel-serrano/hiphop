882c5bb5ca44b390c4c9729511c100213cfc56d9
Colin Vidal <colin.vidal@inria.fr>
2017-10-20 15:22:45 +0200

    Preprocessor: avoid to override the INPUT file.

--- preprocessor/preprocessor.js
+++ preprocessor/preprocessor.js
@@ -1,15 +1,19 @@
 #!/usr/local/bin/hop --no-server
 "use hopscript"
 
+const path = require("path");
 const fs = require("fs");
 const parser = require("./parser");
 const lexer = require("./lexer");
-const inputFile = process.env.INPUT;
-const outputFile = process.env.OUTPUT;
+const inputFile = path.resolve(process.env.INPUT);
+const outputFile = path.resolve(process.env.OUTPUT);
 
 if (!inputFile || !outputFile) {
    console.error(`Usage: INPUT=<input-file> OUTPUT=<output-file> ${process.argv[0]}`);
    process.exit(1);
+} else if (inputFile === outputFile) {
+   console.error(`Error: INPUT and OUTPUT are the same file.`);
+   process.exit(2);
 }
 
 const inputBuffer = fs.readFileSync(inputFile, "utf8");
