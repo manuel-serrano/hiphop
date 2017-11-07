539368b11a85cdee93edacf7fbd8a04c18ecbecb
Colin Vidal <colin.vidal@inria.fr>
2017-11-07 14:32:24 +0100

    Examples: rewrite hiphopfm with new syntax

--- examples/hiphopfm/hiphopfm-new-syntax.js
+++ examples/hiphopfm/hiphopfm-new-syntax.js
@@ -14,11 +14,11 @@ service hiphopfm() {
 	  var genresView;
 	  var detailView;
 
-          const random = function(max) {
+          function random(max) {
 	     return Math.floor(Math.random() * max);
 	  }
 
-	  const fma = function(dataset, callback, opt={}) {
+	  function fma(dataset, callback, opt={}) {
 	     let addr = `https://freemusicarchive.org/api/get/${dataset}.json`;
 	     addr += "?api_key=49T6MKJCF929XKN3";
 	     addr += "&limit=50";
@@ -27,7 +27,7 @@ service hiphopfm() {
 	     }
 
 	     let req = new XMLHttpRequest();
-	     req.onreadystatechange = () => {
+	     req.onreadystatechange = function() {
 		if (req.readyState == 4 && req.status == 200) {
 		   callback(JSON.parse(req.response).dataset);
 		}
@@ -36,12 +36,12 @@ service hiphopfm() {
 	     req.send();
 	  }
 
-	  const startGenre = function(genre) {
+	  function startGenre(genre) {
 	     machine.inputAndReact("GENRE", genre);
 	     toogleGenreDetailView();
 	  }
 
-	  const toogleGenreDetailView = function() {
+	  function toogleGenreDetailView() {
 	     let toogleBtn = document.getElementById("genreSelectionButton");
 	     if (genresView.style.display == "none") {
 		genresView.style.display = "block";
@@ -55,12 +55,12 @@ service hiphopfm() {
 	     }
 	  }
 
-	  const getRandomTrackFromGenre = function(genre, callback) {
-	     fma("albums", albums => {
-		fma("tracks", tracks => {
+	  function getRandomTrackFromGenre(genre, doneReact) {
+	     fma("albums", function(albums) {
+		fma("tracks", function(tracks) {
 		   let track = tracks[random(tracks.length)];
 		   if (track) {
-		      callback(track);
+		      doneReact(track);
 		   } else {
 		      getRandomTrackFromGenre(genre, callback);
 		   }
@@ -68,113 +68,108 @@ service hiphopfm() {
 	     }, {genre_handle: genre.genre_handle});
 	  }
 
+	  function getBio(artistId, doneReact) {
+	     fma("artist", function(data) { doneReact(data[0].artist_bio), {artist_id: artistId} });
+	  }
+
+	  function getDisco(artistId, doneReact) {
+	     fma("albums", function(data) { doneReact(data), {artist_id: artistId} });
+	  }
+
 	  window.onload = function() {
 	     const hh = require("hiphop");
-	     const tl = hh.timelib;
 
-	     const hhAudioModule = function() {
-		let audioMap = [];
-		let curId;
-		return <hh.module __hh_reserved_debug_name__="hiphop-audio"
-		                  SRC PLAYPAUSE SEEKTO
-                                  PAUSED TRACKENDED POSITION DURATION>
-		  <hh.trap ENDED>
-		    <hh.parallel>
-		      <hh.suspend toggleSignal=PLAYPAUSE emitWhenSuspended=PAUSED>
-			<hh.parallel>
-			  <hh.sequence>
-			    <hh.exec TRACKENDED
-                              apply=${function() {
-				 let audio = new Audio(this.value.SRC);
-				 audio.play()
-				 audio.addEventListener("ended", () => {
-				    this.notifyAndReact("ended");
-				 });
-				 audio.addEventListener("error", () => {
-				    this.notifyAndReact("error");
-				 });
-				 audioMap[this.id] = audio;
-				 curId = this.id;
-		              }}
-			      res=${function() {
-				 audioMap[this.id].play();
-			      }}
-			      susp=${function() {
-				 audioMap[this.id].pause();
-			      }}
-			      kill=${function() {
-				 audioMap[this.id].pause();
-				 delete audioMap[this.id];
-			      }}/>
-                            <hh.exit ENDED/>
-			  </hh.sequence>
-			  <hh.sequence>
-			    <tl.pauseAndReact/>
-			    <hh.exec DURATION apply=${function() {
-			       audioMap[curId].addEventListener("durationchange", () => {
-				  this.notifyAndReact(audioMap[curId].duration);
-			       });
-			    }}/>
-			  </hh.sequence>
-			  <tl.interval value="1000">
-			    <hh.emit POSITION apply=${function() {
-			       return audioMap[curId].currentTime;
-			    }}/>
-			  </tl.interval>
-			</hh.parallel>
-		      </hh.suspend>
-		      <hh.every SEEKTO>
-			<hh.atom apply=${function() {
-			   audioMap[curId].currentTime = this.value.SEEKTO;
-			}}/>
-		      </hh.every>
-		    </hh.parallel>
-		  </hh.trap>
-		</hh.module>
+	     function hhSleepModule(ms) {
+		let id;
+		return MODULE {
+		   EXEC id = setTimeout(DONEREACT, ms)
+		      ONSUSP clearTimeout(id)
+		      ONRES id = setTimeout(DONEREACT, ms)
+		      ONKILL clearTimeout(id);
+		}
 	     }
 
-	     machine = new hh.ReactiveMachine(
-		<hh.module GENRE NEXT PLAYPAUSE SEEKTO
-		           PAUSED TRACKENDED POSITION DURATION TRACK BIO DISCO>
-		  <hh.every immediate GENRE>
-		    <hh.loop>
-		      <hh.trap NEXTTRACK>
-			<hh.parallel>
-			  <hh.sequence>
-			    <hh.await NEXT/>
-			    <hh.exit NEXTTRACK/>
-			  </hh.sequence>
-			  <hh.sequence>
-			    <hh.emit BIO DISCO value=" "/>
-			    <hh.exec TRACK apply=${function() {
-			       getRandomTrackFromGenre(this.value.GENRE,
-						       this.notifyAndReact);
-			    }}/>
-			    <hh.parallel>
-			      <hh.local SRC=${{initApply: function() {
-				 return this.value.TRACK.track_url + "/download"}}}>
-				<hh.run module=${hhAudioModule()}/>
-				<hh.exit NEXTTRACK/>
-			      </hh.local>
-			      <hh.exec BIO apply=${function() {
-				 fma("artists", data => {
-				    this.notifyAndReact(data[0].artist_bio);
-				 }, {artist_id: this.value.TRACK.artist_id})
-			      }}/>
-			      <hh.exec DISCO apply=${function() {
-				 fma("albums", data => {
-				    this.notifyAndReact(data);
-				 }, {artist_id: this.value.TRACK.artist_id})
-			      }}/>
-			    </hh.parallel>
-			  </hh.sequence>
-			</hh.parallel>
-		      </hh.trap>
-		    </hh.loop>
-		  </hh.every>
-		</hh.module>
-	     );
-	     machine.react();
+	     function hhAudioModule() {
+		let audio = new Audio();
+		let errorListener = null;
+		let endedListener = null;
+
+		function start(src, doneReact) {
+		   audio.src = src;
+		   audio.play()
+
+		   if (errorListener) {
+		      audio.removeEventListener("error", errorListener);
+		   }
+		   errorListener = function() { doneReact("ended") };
+		   audio.addEventListener("error", errorListener);
+
+		   if (endedListener) {
+		      audio.removeEventListener("ended", endedListener);
+		   }
+		   endedListener = function() { doneReact("error") };
+		   audio.addEventListener("ended", endedListener);
+		}
+
+		return MODULE HiphopAudio {
+		   IN src, playPause, seekTo;
+		   OUT paused, trackEnded, position, duration;
+		   TRAP Ended {
+		      FORK {
+			 SUSPEND TOGGLE(NOW(playPause)) EMITWHENSUSPENDED paused {
+			    FORK {
+			       EXECEMIT trackEnded start(VAL(src), DONEREACT)
+			          ONRES audio.play()
+			          ONSUSP audio.pause()
+			          ONKILL audio.pause();
+			       EXIT Ended;
+			    } PAR {
+			       LOOP {
+				  RUN(hhSleepModule(1000));
+				  EMIT position(audio.currentTime);
+				  EMIT duration(audio.duration);
+			       }
+			    }
+			 }
+		      } PAR {
+			 EVERY(NOW(seekTo)) {
+			    ATOM {
+			       audio.currentTime = VAL(seekTo);
+			    }
+			    EMIT position(audio.currentTime);
+			 }
+		      }
+		   }
+		}
+	     }
+
+	     machine = new hh.ReactiveMachine(MODULE {
+		IN genre, next, playPause, seekTo;
+		OUT paused, trackEnded, position, duration, track, bio, disco;
+		EVERY IMMEDIATE(NOW(genre)) {
+		   LOOP {
+		      TRAP NextTrack {
+			 FORK {
+			    AWAIT(NOW(next));
+			    EXIT NextTrack;
+			 } PAR {
+			    EMIT bio(" "), disco(" ");
+			    EXECEMIT track getRandomTrackFromGenre(VAL(genre), DONEREACT);
+			    FORK {
+			       LOCAL src(VAL(track).track_url + "/download") {
+				  RUN(hhAudioModule());
+				  EXIT NextTrack;
+			       }
+			    } PAR {
+			       EXECEMIT bio getBio(VAL(track).artist_id, DONEREACT);
+			    } PAR {
+			       EXECEMIT disco getDisco(VAL(track).artist_id, DONEREACT);
+			    }
+			 }
+		      }
+		   }
+		}
+	     });
 	     machine.debuggerOn("debug");
 
 	     controlsView = document.getElementById("controlsView");
@@ -183,7 +178,7 @@ service hiphopfm() {
 	     controlsView.className = "hidden";
 	     detailView.style.display = "none";
 
-	     fma("genres", genres => {
+	     fma("genres", function(genres) {
 	     	genres.forEach(genre => {
 	     	   let button = <button id="genreButton" onclick=~{startGenre(genre)}>
 	     	     ${genre.genre_title}
