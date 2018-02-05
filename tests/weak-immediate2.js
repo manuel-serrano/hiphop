0458ea8c74ab8c3409d293a61e23e31cf07895cc
Colin Vidal <colin.vidal@inria.fr>
2018-02-05 11:22:39 +0100

    Compiler: enable sweep optimization by default (constant folding and buffer elimination)

---
+++ tests/weak-immediate2.js
@@ -0,0 +1,17 @@
+"use hopscript"
+
+var hh = require("hiphop");
+
+var m =
+    <hh.module S=${{accessibility: hh.IN}} O F W>
+      <hh.weakabort immediate S>
+	<hh.loop>
+	  <hh.emit O/>
+	  <hh.pause/>
+	  <hh.emit W/>
+	</hh.loop>
+      </hh.weakabort>
+      <hh.emit F/>
+    </hh.module>
+
+exports.prg = new hh.ReactiveMachine(m, "wabortimmediate")
