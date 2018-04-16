9249d9e376fe171fdc2d940d8736a71b476a0fd5
Colin Vidal <colin.vidal@inria.fr>
2018-04-16 13:05:51 +0200

    Abstract syntax: add `direction` as alias for `accessibility` in signal properties.

--- tests/abcro.js
+++ tests/abcro.js
@@ -2,8 +2,8 @@
 
 var hh = require("hiphop");
 
-var inSig = {accessibility: hh.IN};
-var outSig = {accessibility: hh.OUT};
+var inSig = {direction: hh.IN};
+var outSig = {direction: hh.OUT};
 
 var prg =
     <hh.module A=${inSig} B=${inSig} C=${inSig} R=${inSig} O=${outSig}>
