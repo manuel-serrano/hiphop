0d5c310b97d1c1ff9f8303788a80313fc39b6f26
Manuel Serrano <Manuel.Serrano@inria.fr>
2020-05-22 17:51:15 +0200

    Beginning of the Android example

---
+++ examples/android/README.md
@@ -0,0 +1,2 @@
+This example is meant to run on an Android device, using the
+Hop's `hopdroid` module.
---
+++ examples/android/android.js
@@ -0,0 +1,55 @@
+"use hiphop"
+"use hopscript"
+
+const fs = require( "fs" );
+const path = require( "path" );
+const hopdroid = require( hop.hopdroid );
+
+const phone = new hopdroid.phone();
+
+hiphop module Alarm( alarm, minutes ) {
+   async {
+      this.intv = 
+	 setTimeout( () => this.react( {[alarm.signame]: true} ), 
+	    1000 * 60 * minutes );
+   }
+}
+
+hiphop module autoReply( in smsdelivered, smsreceived, autoreply ) {
+   every( smsreceived.now && autoreply.nowval > 0 ) {
+      let no = smsreceived.nowval[ 0 ];
+      signal alarm;
+      abort( smsdelivered.now && smsdelivered.nowval[ 0 ] === no ||
+	     autoreply.now && autoreply.nowval === 0 ) {
+	 fork {
+	    run Alarm( alarm, minutes = autoreply.value );
+	 } par {
+	    await alarm.now;
+	    hop { 
+	       phone.smsSend( no, "I'm busy. I will answer as soon as I can" );
+	       hop.broadcast( "autoreply", no );
+ 	    }
+	 }
+      }
+   }
+}
+
+phone.bindEvent( "smsdelivered", phone );
+phone.bindEvent( "smsreceived", phone );
+
+service android() {
+   const con = <div/>;
+   return <html>
+     <script>
+       server.addEventListener( "autoreply", e => ${con}.innerHTML = e.value );
+     </script>
+     <input id="autoreply"
+	    type=checkbox 
+	    onclick=~{
+	       ${service (v) { mach.react( { autoreply: v } ) }}( this.value ? dom.getElementById( delay ).value : 0 ).post() }/>
+     <label for="autoreply">delay</label>
+     <input id="delay" type=range min=1 max=10/> 
+     <label for="delay">delay</label>
+     ${ con }
+   </html>
+}
