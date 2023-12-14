b453f53cba81d151313558451096ddc154f972f1
Manuel Serrano <Manuel.Serrano@inria.fr>
2023-12-14 09:36:01 +0100

    Fix the multiple evaluation of signames when the form this[expr].now is used.

---
+++ tests/dynamic-signames.hh.js
@@ -0,0 +1,14 @@
+"use @hop/hiphop"
+"use hopscript"
+
+import { ReactiveMachine } from "@hop/hiphop";
+
+hiphop module prg() {
+   host { ; }
+}
+
+export const mach = new ReactiveMachine(prg);
+
+// the parser used to fail because after the parsing of the template
+// `red` the parser cursor was positioned after the ']' character.
+mach[`red`] = { init: () => [], combine: (x, y) => x };
