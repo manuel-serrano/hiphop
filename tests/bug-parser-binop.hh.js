067cf3a0e67e498bdb8d2bc045bd4fb0da5ba06a
Manuel Serrano <Manuel.Serrano@inria.fr>
2023-12-14 10:52:12 +0100

    New test

---
+++ tests/bug-parser-binop.hh.js
@@ -0,0 +1,17 @@
+"use @hop/hiphop"
+"use hopscript"
+
+import { ReactiveMachine } from "@hop/hiphop";
+
+hiphop module prg() {
+   host { ; }
+}
+
+export const mach = new ReactiveMachine(prg);
+
+// the parser used to fail because after the parsing of the template
+// `red` the parser cursor was positioned after the ']' character.
+if ("red" in mach) {
+   console.log("should not be here");
+}
+
