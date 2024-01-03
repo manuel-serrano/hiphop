69bfb1fe08e00f2ff0cb0011a59fa63f7e373b74
Manuel Serrano <Manuel.Serrano@inria.fr>
2024-01-03 08:29:38 +0100

    Promise test again

--- test/promise.hh.js
+++ test/promise.hh.js
@@ -2,11 +2,11 @@ import * as hh from "@hop/hiphop";
 
 hiphop module prg() {
    out res, rej;
-   emit res("machine promise resolved");
+   emit res("promise resolved");
 }
 
 export const mach = new hh.ReactiveMachine(prg, "promise");
 mach.outbuf = "";
 
-mach.batchPromise = mach.promise().then(v => mach.outbuf += v);
+mach.batchPromise = mach.promise().then(v => mach.outbuf += mach.name + " " + v);
 mach.react();
