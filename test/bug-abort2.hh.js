77efc203ee27e05d0e712c8d789558a3fb75f844
Manuel Serrano <Manuel.Serrano@inria.fr>
2025-01-30 19:21:40 +0100

    New abort test

---
+++ test/bug-abort2.hh.js
@@ -0,0 +1,33 @@
+"use @hop/hiphop"
+"use hopscript"
+
+import { ReactiveMachine } from "@hop/hiphop";
+
+hiphop module prg() {
+   in A;
+   out C;
+   out D;
+
+   abort (A.now) {
+      ;
+   }
+   emit C(123);
+   yield;
+   emit D(456);
+}
+
+export const mach = new ReactiveMachine(prg);
+mach.outbuf = "";
+mach.addEventListener("C", e => mach.outbuf += ("C: " + e.nowval + "\n"));
+mach.addEventListener("D", e => mach.outbuf += ("D: " + e.nowval + "\n"));
+mach.outbuf += ("1\n");
+mach.react({});
+mach.outbuf += ("2\n");
+mach.react({A:111});
+mach.outbuf += ("3\n");
+mach.react();
+mach.outbuf += ("4\n");
+mach.react();
+mach.outbuf += ("5\n");
+
+console.error("BUG-ABORT should have failed.");
