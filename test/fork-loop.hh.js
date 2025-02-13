3183f7763cb36c0101768c63873b12550f302a3d
Manuel Serrano <Manuel.Serrano@inria.fr>
2025-02-13 17:40:38 +0100

    New compiler bug fixes (still incomplete)

---
+++ test/fork-loop.hh.js
@@ -0,0 +1,21 @@
+import * as hh from "@hop/hiphop";
+
+  const prg = hiphop module() {
+   in A, B, R; out O;
+
+   loop {
+      pragma { mach.outbuf += ("loop" + mach.age() + "\n"); }
+      fork {
+	 pragma { mach.outbuf += ("S1\n"); }
+      }
+     yield;
+   }
+}
+ 
+export const mach = new hh.ReactiveMachine(prg, "FORK-LOOP");
+mach.outbuf = "";
+
+mach.react();
+mach.react();
+mach.react();
+mach.react();
