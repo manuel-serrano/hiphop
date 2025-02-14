6c99f42fa4f91685070fd2f1d09a27114e697a57
Manuel Serrano <Manuel.Serrano@inria.fr>
2025-02-14 14:28:04 +0100

    Better visualization tools

--- tools/nets2dot.mjs
+++ tools/nets2dot.mjs
@@ -4,7 +4,7 @@
 /*    -------------------------------------------------------------    */
 /*    Author      :  manuel serrano                                    */
 /*    Creation    :  Thu Nov 30 07:21:01 2023                          */
-/*    Last change :  Tue Feb 11 08:41:59 2025 (serrano)                */
+/*    Last change :  Fri Feb 14 09:56:22 2025 (serrano)                */
 /*    Copyright   :  2023-25 manuel serrano                            */
 /*    -------------------------------------------------------------    */
 /*    Generate a DOT file from a netlist.                              */
@@ -33,6 +33,52 @@ const IN = 1;    //  001, is in ? === accessibility & 1
 const INOUT = 3; //  011
 const OUT = 2;   //  010, is out ? === accessibility & 2
 const LOCAL = 4; //  100
+
+
+/*---------------------------------------------------------------------*/
+/*    locPredefinedColors ...                                          */
+/*---------------------------------------------------------------------*/
+const locPredefinedColors = [
+   "#B79762", "#004D43", "#8FB0FF", "#997D87", "#5A0007", "#809693",
+   "#FEFFE6", "#1B4400", "#4FC601", "#3B5DFF", "#4A3B53", "#FF2F80",
+   "#61615A", "#BA0900", "#6B7900", "#00C2A0", "#FFAA92", "#FF90C9",
+   "#B903AA", "#D16100", "#DDEFFF", "#000035", "#7B4F4B", "#A1C299",
+   "#300018", "#0AA6D8", "#013349", "#00846F", "#372101", "#FFB500",
+   "#C2FFED", "#A079BF", "#CC0744", "#C0B9B2", "#C2FF99", "#001E09",
+   "#00489C", "#6F0062", "#0CBD66", "#EEC3FF", "#456D75", "#B77B68",
+   "#7A87A1", "#788D66", "#885578", "#FAD09F", "#FF8A9A", "#D157A0",
+   "#BEC459", "#456648", "#0086ED", "#886F4C", "#34362D", "#B4A8BD",
+   "#006FA6", "#A30059", "#FFDBE5", "#7A4900", "#0000A6", "#63FFAC",
+   "#00A6AA", "#452C2C", "#636375", "#A3C8C9", "#FF913F", "#938A81",
+   "#575329", "#00FECF", "#B05B6F", "#8CD0FF", "#3B9700", "#04F757",
+   "#C8A1A1", "#1E6E00", "#7900D7", "#A77500", "#6367A9", "#A05837",
+   "#6B002C", "#772600", "#D790FF", "#9B9700", "#549E79", "#FFF69F",
+   "#201625", "#72418F", "#BC23FF", "#99ADC0", "#3A2465", "#922329",
+   "#5B4534", "#FDE8DC", "#404E55", "#0089A3", "#CB7E98", "#A4E804",
+   "#324E72", "#6A3A4C", "#83AB58", "#001C1E", "#D1F7CE", "#004B28",
+   "#C8D0F6", "#A3A489", "#806C66", "#222800", "#BF5650", "#E83000",
+   "#66796D", "#DA007C", "#FF1A59", "#8ADBB4", "#1E0200", "#5B4E51",
+   "#C895C5", "#320033", "#FF6832", "#66E1D3", "#CFCDAC", "#D0AC94",
+   "#000000", "#FFFF00", "#1CE6FF", "#FF34FF", "#FF4A46", "#008941",
+   "#7ED379", "#012C58"
+];
+let locColorIndex = 0;
+				  
+/*---------------------------------------------------------------------*/
+/*    locColors ...                                                    */
+/*---------------------------------------------------------------------*/
+const locColors = {};
+
+/*---------------------------------------------------------------------*/
+/*    nextLocColor ...                                                 */
+/*---------------------------------------------------------------------*/
+function nextLocColor() {
+   locColorIndex++;
+   if (locColorIndex >= locPredefinedColors.length) {
+      locColorIndex = 0;
+   }
+   return locPredefinedColors[locColorIndex];
+}
 
 /*---------------------------------------------------------------------*/
 /*    main ...                                                         */
@@ -86,6 +132,17 @@ function main(argv) {
       }
    }
    
+   function netLocColor(net) {
+      const i = net.$loc.pos + 1000 * net.$ast.charCodeAt(0);
+      
+      if (i in locColors) {
+	 return locColors[i];
+      } else {
+	 locColors[i] = nextLocColor();
+	 return locColors[i];
+      }
+   }
+   
    function escape(str) {
       return str.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;");
    }
@@ -130,17 +187,18 @@ function main(argv) {
    info.nets.forEach(net => {
       const typ = netType(net);
       const id = td({content: `${net.id} [${typ}:${net.lvl}]${(net.$sweepable ? "" : "*")}`});
-      const name = net.$name ? tr([td({content: net.$name})]) : "";
+      const name = net.$name ? tr([td({content: escape(net.$name)})]) : "";
       const sigs = net.signals ? tr([td({content: "[" + net.signals + "]"})]) : (net.signame ? tr([td({content: net.signame})]) : "");
-      const action = net.$action ? tr([td({content: escape(net.$action)})]) : (net.value !== undefined? tr([td({content: net.value})]) : "");
+      const action = net.$action ? tr([td({content: font({content: escape(net.$action)})})]) : (net.value !== undefined? tr([td({content: font({content: net.value})})]) : "");
       const fanouts = net.fanout.map((n, i, arr) => tr([port(n, i, `${small(n.id)} &bull;`)]))
       const fanins = net.fanin.map((n, i, arr) => tr([port(n, i + net.fanout.length, `&bull; ${small(n.id)}`, "left")]))
       const fans = table({rows: [tr([td({content: table({rows: fanins})}), td({content: table({rows: fanouts})})])]});
       const header = table({bgcolor: netColor(net), rows: [tr([id]), name]});
-      const file = table({cellpadding: 4, rows: [tr([td({content: font({content: `${basename(net.$loc.filename)}:${net.$loc.pos}`})})]), sigs, action]});;
+      const file = table({cellpadding: 4, rows: [tr([td({content: `${basename(net.$loc.filename)}:${net.$loc.pos}`})]), sigs, action]});;
       const node = table({rows: [tr([td({content: header})]), tr([td({content: file})]), tr([td({align: "center", content: fans})])]});
+      const colorNode = table({cellpadding: 6, bgcolor: netLocColor(net), rows: [tr([td({content: node})])]});
       
-      console.log(`${net.id} [fontname = "Courier New" shape = "none" label = <${node}>];`);
+      console.log(`${net.id} [fontname = "Courier New" shape = "none" label = <${colorNode}>];`);
    });
       
 
