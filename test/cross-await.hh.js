96c2ca9f61c38228768ea545be98209769f896f6
Manuel Serrano <Manuel.Serrano@inria.fr>
2025-03-07 18:16:36 +0100

    Towards native compiler 29 failures

--- test/cross-await.hh.js
+++ test/cross-await.hh.js
@@ -1,19 +1,16 @@
-"use @hop/hiphop";
-"use hopscript";
-
 import * as hh from "@hop/hiphop";
 
 hiphop module prg() {
    inout A, B, END1, END2;
    fork {
       emit A();
-      await immediate( B );
+      await immediate(B);
       emit END1();
    } par {
       emit B();
-      await immediate( B );
+      await immediate(B);
       emit END2();
    }
 }
 
-export const mach = new hh.ReactiveMachine( prg, "crossawait" );
+export const mach = new hh.ReactiveMachine(prg, "crossawait");
