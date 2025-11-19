5a3f9e2946c9d84a047cee49809309a2830f94e1
Manuel Serrano <Manuel.Serrano@inria.fr>
2025-11-19 10:31:37 +0100

    Default random testing configutation

--- testrandom/config.mjs
+++ testrandom/config.mjs
@@ -3,7 +3,7 @@
 /*    -------------------------------------------------------------    */
 /*    Author      :  Manuel Serrano                                    */
 /*    Creation    :  Tue Nov 18 07:21:33 2025                          */
-/*    Last change :  Wed Nov 19 07:54:21 2025 (serrano)                */
+/*    Last change :  Wed Nov 19 10:31:25 2025 (serrano)                */
 /*    Copyright   :  2025 Manuel Serrano                               */
 /*    -------------------------------------------------------------    */
 /*    testrandom configuration                                         */
@@ -13,6 +13,6 @@ export const COUNT = parseInt(process.env.HIPHOP_RT_COUNT) || 5000;
 export const LOOPSAFE = process.env?.HIPHOP_RT_LOOPSAFE !== "false";
 export const REASON = process.env?.HIPHOP_RT_REASON === "true";
 export const VERBOSE = parseInt(process.env.HIPHOP_RT_VERBOSE) || 0;
-export const MACHINES = process.env.HIPHOP_RT_MACHINES?.split(" ") ?? ["default", "colin" ];
+export const MACHINES = process.env.HIPHOP_RT_MACHINES?.split(" ") ?? ["default", "native" ];
 export const MINSIZE = parseInt(process.env?.HIPHOP_RT_MINSIZE ?? "5");
 export const MAXSIZE = parseInt(process.env?.HIPHOP_RT_MAXSIZE ?? "20");
