6d135cc32be9496ce08bfe3108365e7fac7307db
Manuel Serrano <Manuel.Serrano@inria.fr>
2025-12-12 15:56:54 +0100

    New tests for preparing reincarnation

---
+++ test/reincar3.hh.js
@@ -0,0 +1,21 @@
+import * as hh from "@hop/hiphop";
+
+const events = [{"I":56},{}];
+
+const prg = hiphop module() {
+   inout I, O;
+   fork {
+      pragma { mach.outbuf += ("O " + mach.age() + "\n"); }
+   } par {
+      yield;
+   }
+   pragma { mach.outbuf += ("L "+ mach.age() + "\n"); }
+}
+
+const opts = {"name":"rnc", native: "no"};
+export const mach = new hh.ReactiveMachine(prg, opts);
+mach.outbuf = "";
+
+events.forEach((e, i) => {
+   mach.react();
+});
